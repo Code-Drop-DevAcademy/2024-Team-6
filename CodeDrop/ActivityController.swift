84066a49d5c4460dbb928401075bfe3e5b037613
seongahshin <cindy903c@naver.com>
2024-06-15 05:24:59 +0900

    feat: 사진 전송 기능 구현 #3

---
+++ CodeDrop/ActivityController.swift
@@ -0,0 +1,33 @@
+//
+//  ActivityController.swift
+//  CodeDrop
+//
+//  Created by 신승아 on 6/15/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct ActivityViewController: UIViewControllerRepresentable {
+  var activityItems: [Any]
+  var applicationActivities: [UIActivity]? = nil
+  @Environment(\.presentationMode) var presentationMode
+  
+  func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>
+  ) -> UIActivityViewController {
+    let controller = UIActivityViewController(
+      activityItems: activityItems,
+      applicationActivities: applicationActivities
+    )
+    controller.completionWithItemsHandler = { (activityType, completed, returnedItems, error) in
+      self.presentationMode.wrappedValue.dismiss()
+    }
+    return controller
+  }
+  
+  func updateUIViewController(
+    _ uiViewController: UIActivityViewController,
+    context: UIViewControllerRepresentableContext<ActivityViewController>
+  ) {}
+}
--- CodeDrop/MyCardView.swift
+++ CodeDrop/MyCardView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct MyCardView: View {
+    @State var isSharedPresented = false
+    
     var body: some View {
         VStack {
             HStack {
@@ -22,67 +24,11 @@ struct MyCardView: View {
             }
             .padding()
             
-            ZStack {
-                Rectangle()
-                    .foregroundStyle(.orange)
-                    .cornerRadius(12)
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("정해시")
-                            .font(.title)
-                            .bold()
-                            .padding(.leading, 20)
-                            .padding(.top, 20)
-                            .foregroundStyle(.white)
-                        
-                        Spacer()
-                            
-                        Image(systemName: "ellipsis")
-                            .font(.title)
-                            .padding(.top, 20)
-                            .padding(.trailing, 20)
-                            .foregroundStyle(.white)
-                        
-                    }
-                    
-                    
-                    Spacer()
-                    
-                    Text("관심사")
-                        .bold()
-                        .padding(.leading, 20)
-                        .foregroundStyle(.white)
-                    
-                    HStack {
-                        TagView(text: "운동")
-                        TagView(text: "고양이")
-                        TagView(text: "의류")
-                    }
-                    .padding(.bottom, 20)
-                    .padding(.leading, 20)
-                    
-                    Text("좋아하는 음식")
-                        .bold()
-                        .padding(.leading, 20)
-                        .foregroundStyle(.white)
-                    
-                    HStack {
-                        TagView(text: "떡볶이")
-                        TagView(text: "피자")
-                        TagView(text: "치킨")
-                    }
-                    .padding(.bottom, 20)
-                    .padding(.leading, 20)
-                }
-                
-            }
-            .padding(.leading, 20)
-            .padding(.trailing, 20)
-            .frame(height: 500)
+            cardView
             
             Button(action: {
                 // Action for sharing the card
+                self.isSharedPresented = true
             }) {
                 HStack {
                     Image(systemName: "square.and.arrow.up")
@@ -96,8 +42,74 @@ struct MyCardView: View {
                 .background(.black)
                 .cornerRadius(10)
             }
+            .sheet(
+                isPresented: $isSharedPresented,
+                onDismiss: { print("Dismiss") },
+                content: { ActivityViewController(activityItems: [cardView.asUIImage(size: CGSize(width: 353, height: 600))]) }
+            )
             .padding()
         }
+    }
+    
+    private var cardView: some View {
+        ZStack {
+            Rectangle()
+                .foregroundStyle(.orange)
+                .cornerRadius(12)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("정해시")
+                        .font(.title)
+                        .bold()
+                        .padding(.leading, 20)
+                        .padding(.top, 20)
+                        .foregroundStyle(.white)
+                    
+                    Spacer()
+                        
+                    Image(systemName: "ellipsis")
+                        .font(.title)
+                        .padding(.top, 20)
+                        .padding(.trailing, 20)
+                        .foregroundStyle(.white)
+                    
+                }
+                
+                
+                Spacer()
+                
+                Text("관심사")
+                    .bold()
+                    .padding(.leading, 20)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    TagView(text: "운동")
+                    TagView(text: "고양이")
+                    TagView(text: "의류")
+                }
+                .padding(.bottom, 20)
+                .padding(.leading, 20)
+                
+                Text("좋아하는 음식")
+                    .bold()
+                    .padding(.leading, 20)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    TagView(text: "떡볶이")
+                    TagView(text: "피자")
+                    TagView(text: "치킨")
+                }
+                .padding(.bottom, 20)
+                .padding(.leading, 20)
+            }
+            
+        }
+        .padding(.leading, 20)
+        .padding(.trailing, 20)
+        .frame(height: 500)
     }
 }
 

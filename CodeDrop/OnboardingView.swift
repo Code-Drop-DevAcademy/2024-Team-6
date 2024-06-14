f55cf1fb3f95fb5669b2806ae740f821213d7192
HyunJaeyeon <bomul0109@gmail.com>
2024-06-15 08:01:14 +0900

    feat: 온보딩 시간 및 Navigation 설정

--- CodeDrop/OnboardingView.swift
+++ CodeDrop/OnboardingView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    private var onboardingTime = 2.0
+    
+    @State private var isNavigate: Bool = false
+    
     var body: some View {
+        NavigationStack{
             Image("Splash")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
+            
+            NavigationLink(
+                destination: ContentView()
+                    .navigationBarHidden(true),
+                isActive: $isNavigate
+            ) {
+                EmptyView()
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + onboardingTime) {
+                self.isNavigate = true
+            }
+        }
     }
 }
 
-#Preview {
-    OnboardingView()
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
 }
+

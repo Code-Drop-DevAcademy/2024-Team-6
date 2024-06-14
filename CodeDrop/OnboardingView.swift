95cf4e7aa33499c160cb1fdc74cc09a3350217ee
HyunJaeyeon <bomul0109@gmail.com>
2024-06-15 07:50:47 +0900

    feat: 온보딩 배경 이미지 추가

---
+++ CodeDrop/OnboardingView.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingView.swift
+//  CodeDrop
+//
+//  Created by Hyun Jaeyeon on 6/15/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+            Image("Splash")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
